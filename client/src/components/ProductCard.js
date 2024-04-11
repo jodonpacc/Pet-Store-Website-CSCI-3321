5ebc0767b5c50c48ca58abc0b80b8cbf6dac8f30
LeviKle <leuykhiem@gmail.com>
2024-04-11 13:26:55 -0500

    home page connects to database

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,17 +1,17 @@
 // The orange rounded rectangle with product image and product title seen on the homepage of the mockup
 import "./Card.css"
 
-function ProductCard({id, img, title, destination}) {
+function ProductCard({ id, imgname, title, destination }) {
 
+    const imglink = "../assets/images/" + imgname
     const gotoDest = (e, destination) => {
         window.location.href = destination;
-    }  
+    }
 
     return (
         <div className="product-card" id={id}>
             <div className="image-backer">
-                <img>{img}</img>
-                image
+                <img src={imglink}></img>
             </div>
             <div className="product-card-title" onClick={(e) => gotoDest(e, destination)}>{title}</div>
         </div>
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,32 +1,50 @@
 import ProductCard from "../../components/ProductCard";
+import React, { useState, useEffect } from 'react';
 import CategoryCard from "../../components/CategoryCard";
 import NavigationBar from "../../components/NavigationBar";
 import './Home.css'
 import axios from 'axios';
 
-function HomePage({}) {
+function HomePage({ }) {
 
     // This line must be present on every main page so that session information is circulated properly
     axios.defaults.withCredentials = true;
+    const [products, setProducts] = useState([]);
+
+    // useEffect make the Axios request when the component mounts
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:9000/product/allProducts');
+                setProducts(response.data); // Store the fetched data in state
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData(); // Call the fetchData function
+    }, []);
+    console.log(products)
 
     return (
         <div id="home-page">
-            <NavigationBar/>
+            <NavigationBar />
             <div id="padded-center">
-                <div id="featured-categories">
-                    <CategoryCard title={"Product Category"}/>
-                    <CategoryCard title={"Product Category"}/>
-                    <CategoryCard title={"Product Category"}/>
-                </div>
+                {/* not supporting categories
+                 <div id="featured-categories">
+                    <CategoryCard title={"Product Category"} />
+                    <CategoryCard title={"Product Category"} />
+                    <CategoryCard title={"Product Category"} />
+                </div> */}
                 <div id="all-products-label">
                     All Products:
                 </div>
                 <div className="home-fake-hr"></div>
                 <div id="product-array">
-                    <ProductCard title={"Product Card"} destination="/product"/>
-                    <ProductCard title={"Product Card"} destination="/product"/>
-                    <ProductCard title={"Product Card"} destination="/product"/>
-                    <ProductCard title={"Product Card"} destination="/product"/>
+                    {products.map((product, index) => (
+                        //key is just something React requires
+                        <ProductCard key={index} imgname={product.img_filename} title={product.title} destination={"/product"} />
+                    ))}
                 </div>
             </div>
         </div>
